a136f57f2a5252ab727c06f5b27d5948c41c9f09
IshtiaqueNafis <nafu22@gmail.com>
2021-11-09 12:11:25 -0500

    Created EventForm

--- src/features/events/eventDashBoard/EventDashBoard.jsx
+++ src/features/events/eventDashBoard/EventDashBoard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid} from "semantic-ui-react";
 import EventList from "./EventList";
+import EventForm from "../eventForm/EventForm";
 
 const EventDashBoard = () => {
     return (
@@ -9,7 +10,7 @@ const EventDashBoard = () => {
                 <EventList/>
             </Grid.Column>
             <Grid.Column width={6}>
-                <h2>Right Columns</h2>
+                <EventForm/>
             </Grid.Column>
         </Grid>
     );
