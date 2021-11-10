32d3418dac1380c087f45d883aaac1ffb0ac58d7
IshtiaqueNafis <nafu22@gmail.com>
2021-11-10 12:17:17 -0500

    created SignInMenu.jsx to signin a user and SignOutMenu.jsx to signout a user

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Button, Container, Menu} from "semantic-ui-react";
 import {NavLink} from "react-router-dom";
+import SignedOutMenu from "./SignedOutMenu";
+import SignInMenu from "./SignInMenu";
 
 const NavBar = ({setFormOpen}) => {
     return (
@@ -10,15 +12,13 @@ const NavBar = ({setFormOpen}) => {
                     {/*note as={NavLink} exact to={'/'} converts this MenuItem to NavLink*/}
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}}/>
                     re-vents
-                </Menu.Item >
+                </Menu.Item>
                 <Menu.Item as={NavLink} to={'/events'} name='Events'/>
                 <Menu.Item as={NavLink} to={'/createEvent'}>
-                    <Button  positive inverted content={'Create Event'}/>
+                    <Button positive inverted content={'Create Event'}/>
                 </Menu.Item>
-                <Menu.Item position="right">
-                    <Button basic inverted content={'Login'}/>
-                    <Button basic inverted content={'Register'} style={{marginLeft: '0.5em'}}/>
-                </Menu.Item>
+                <SignedOutMenu/>
+                <SignInMenu/>
             </Container>
         </Menu>
     );
---
+++ src/features/nav/SignInMenu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Dropdown, Image, Menu} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+
+const SignInMenu = () => {
+    return (
+
+        <Menu.Item position="right">
+            <Image avatar spaced={'right'} src={'/assets/user.png'}></Image>
+            <Dropdown pointing={'top left'} text="BoB">
+                <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to={'/createEvent'} text="Create Event" icon={'plus'}/>
+                    <Dropdown.Item text="My Profile" icon={'user'}/>
+                    <Dropdown.Item text="Sign Out" icon={'power'}/>
+                </Dropdown.Menu>
+            </Dropdown>
+        </Menu.Item>
+
+    );
+};
+
+export default SignInMenu;
