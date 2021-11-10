f709dbfe931ff6521824efeac5e1de55bfd9cc9c
IshtiaqueNafis <nafu22@gmail.com>
2021-11-09 21:37:23 -0500

    added routing on NavBar.jsx

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Button, Container, Menu} from "semantic-ui-react";
+import {NavLink} from "react-router-dom";
 
 const NavBar = ({setFormOpen}) => {
     return (
         <Menu inverted fixed={'top'}>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} exact to={'/'} header>
+                    {/*note as={NavLink} exact to={'/'} converts this MenuItem to NavLink*/}
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: 15}}/>
                     re-vents
-                </Menu.Item>
-                <Menu name='Events'/>
-                <Menu.Item>
-                    <Button onClick={()=>setFormOpen(true)} positive inverted content={'Create Event'}/>
+                </Menu.Item >
+                <Menu.Item as={NavLink} to={'/events'} name='Events'/>
+                <Menu.Item as={NavLink} to={'/createEvent'}>
+                    <Button  positive inverted content={'Create Event'}/>
                 </Menu.Item>
                 <Menu.Item position="right">
                     <Button basic inverted content={'Log in'}/>
