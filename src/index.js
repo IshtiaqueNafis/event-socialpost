41274ac39c72a0ad8ee373045e664c3e1f772fe9
IshtiaqueNafis <nafu22@gmail.com>
2021-11-11 16:29:15 -0500

    cleaned up code and added ScrollToTop.jsx to make page go on top automatically

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {configureStore} from "./redux/store/configureStore";
+import ScrollToTop from "./layout/ScrollToTop";
 
 const store = configureStore()
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+        <ScrollToTop/>
             <App/>
         </BrowserRouter>
     </Provider>,
