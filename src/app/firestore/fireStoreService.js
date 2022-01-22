3c7f8c5d1e3d33ff3be94cad8f397c8ebb4dd2f4
IshtiaqueNafis <nafu22@gmail.com>
2022-01-22 12:10:32 -0500

    created dataFromSnapshot(snapshot) method which converts data from firebase to usable Data

--- src/app/firestore/fireStoreService.js
+++ src/app/firestore/fireStoreService.js
@@ -2,4 +2,27 @@ import firebase from '../config/firebase';
 
 const db = firebase.firestore();
 
+//region getEventsFromFirestore = observer --> gets data fromFireStore.
 export const getEventsFromFirestore = observer => db.collection('events').onSnapshot(observer);
+//endregion
+
+//region dataFromSnapshot(snapshot) returns data on usable format
+export function dataFromSnapshot(snapshot){
+    if(!snapshot.exists) return undefined;
+    const data = snapshot.data();
+
+    for(const prop in data){
+        if(data.hasOwnProperty(prop)){
+            if(data[prop] instanceof firebase.firestore.Timestamp){ // this checks whether date is a child of fireBase.fireStore.TimeStamp
+                data[prop] = data[prop].toDate();
+            }
+        }
+    }
+
+
+    return {
+        ...data,
+        id: snapshot.id,
+    }
+}
+//endregion
--- src/features/events/eventDashBoard/EventDashBoard.jsx
+++ src/features/events/eventDashBoard/EventDashBoard.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Grid} from "semantic-ui-react";
 import EventList from "./EventList";
 import {useSelector} from "react-redux";
 import EventListItemPlaceholder from "./EventListItemPlaceHolder";
 import EventFilters from "./EventFilters";
 import LoadingComponent from "../../../layout/LoadingComponent";
+import {dataFromSnapshot, getEventsFromFirestore} from "../../../app/firestore/fireStoreService";
 
 const EventDashBoard = () => {
 
@@ -12,6 +13,14 @@ const EventDashBoard = () => {
     const {events} = useSelector(state => state.event) // event from selectors.
     const {loading} = useSelector(state => state.async); // async fdrom sleelctr.s
     //endregion
+
+    useEffect(()=>{
+        const unsubscribe = getEventsFromFirestore({
+            next: snapshot => console.log(snapshot.docs.map(docSnapshot => dataFromSnapshot(docSnapshot))),
+            error: error => console.log(error)
+        })
+        return unsubscribe;
+    })
 
     if (loading) return <LoadingComponent/>
 
